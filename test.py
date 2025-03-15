53f1be785630d8c0faac1cf093779bdedb353c60
Toty <kobissatoty@gmail.com>
2025-03-15 13:20:23 +0100

    Suppression des fichiers de test et temporaires

--- test.py
+++
@@ -1,6 +0,0 @@
-connector = MT5FileConnector()
-if connector.connect():
-    response = connector.send_command("PING")
-    print(f"Réponse reçue : {response}")
-else:
-    print("Échec de la connexion à MT5.")
